d974e3eb48b7c23f7840355b89666eb98c55f485
Crackmindedsk <shakumbhar@gmail.com>
2021-06-27 00:38:14 +0530

    editor activity

--- app/src/main/java/sharlene/work/petsfoundation/EditorActivity.kt
+++ app/src/main/java/sharlene/work/petsfoundation/EditorActivity.kt
@@ -1,4 +1,14 @@
 package sharlene.work.petsfoundation
 
-class EditorActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+public class EditorActivity: AppCompatActivity() {
+    private mNameEditText=EditText
+
+    private var mGender=0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.)
+    }
 }
