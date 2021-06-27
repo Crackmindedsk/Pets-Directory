ab61f90614def4f33a9ec934153eaef228450f06
Crackmindedsk <shakumbhar@gmail.com>
2021-06-28 00:01:58 +0530

    pictures

--- app/src/main/java/sharlene/work/petsfoundation/CatalogActivity.kt
+++ app/src/main/java/sharlene/work/petsfoundation/CatalogActivity.kt
@@ -18,6 +18,7 @@ class CatalogActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_catalog)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
