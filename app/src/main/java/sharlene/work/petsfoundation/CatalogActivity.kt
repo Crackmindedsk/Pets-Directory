233023fe35feac6344d90ba7a289d8f00fb34c13
Crackmindedsk <shakumbhar@gmail.com>
2021-06-28 21:21:10 +0530

    Editior Activity

--- app/src/main/java/sharlene/work/petsfoundation/CatalogActivity.kt
+++ app/src/main/java/sharlene/work/petsfoundation/CatalogActivity.kt
@@ -4,8 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class CatalogActivity : AppCompatActivity() {
 
@@ -13,7 +13,7 @@ class CatalogActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_catalog)
 
-        val fab:FloatingActionButton = findViewById(R.id.fab)
+        val fab:View = findViewById(R.id.fab)
         fab.setOnClickListener{
                 val intent= Intent(this,EditorActivity::class.java)
                 startActivity(intent)
--- app/src/main/java/sharlene/work/petsfoundation/EditorActivity.kt
+++ app/src/main/java/sharlene/work/petsfoundation/EditorActivity.kt
@@ -1,16 +1,74 @@
 package sharlene.work.petsfoundation
 
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NavUtils
 
 public class EditorActivity: AppCompatActivity() {
-    private val mNameEditText: EditText
+    private var mNameEditText: EditText?=null
+    private var mBreedEditText: EditText?=null
+    private var mWeightEditText: EditText?=null
+    private var mGenderSpinner: Spinner?=null
     private var mGender=0
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editor)
 
+        mNameEditText= EditText(findViewById(R.id.edit_pet_name))
+        mBreedEditText= EditText(findViewById(R.id.edit_pet_breed))
+        mWeightEditText= EditText(findViewById(R.id.edit_pet_weight))
+        mGenderSpinner=Spinner(findViewById(R.id.spinner_gender))
+
+        setupSpinner()
     }
+
+    private fun setupSpinner() {
+        val genderSpinnerAdapter =ArrayAdapter.createFromResource(this,R.array.array_gender_option,android.R.layout.simple_spinner_item)
+        genderSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
+        mGenderSpinner!!.adapter=genderSpinnerAdapter
+        mGenderSpinner!!.onItemSelectedListener= object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view : View, position: Int, id: Long) {
+                val selection= parent.getItemAtPosition(position) as String
+                if(!TextUtils.isEmpty(selection)){
+                    mGender=if (selection==getString(R.string.gender_male))
+                        1
+                    else if(selection==getString(R.string.gender_female))
+                        2
+                    else
+                        0
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                mGender=0
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_editor,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return  when(item.itemId){
+            R.id.action_save->true
+            R.id.action_delete->true
+            android.R.id.home->{
+                NavUtils.navigateUpFromSameTask(this)
+                return true
+            }
+            else->super.onOptionsItemSelected(item)
+        }
+    }
+
 }
