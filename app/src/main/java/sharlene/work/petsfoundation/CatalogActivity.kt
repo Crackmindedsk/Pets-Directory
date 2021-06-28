f4dcc8c850251f46f479cb950d76e917d4151d94
Crackmindedsk <shakumbhar@gmail.com>
2021-06-28 19:02:07 +0530

    Catalog program

--- app/src/main/java/sharlene/work/petsfoundation/CatalogActivity.kt
+++ app/src/main/java/sharlene/work/petsfoundation/CatalogActivity.kt
@@ -1,43 +1,29 @@
 package sharlene.work.petsfoundation
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.google.android.material.snackbar.Snackbar
-import sharlene.work.petsfoundation.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class CatalogActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_catalog)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        val fab:FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener{
+                val intent= Intent(this,EditorActivity::class.java)
+                startActivity(intent)
         }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_catalog, menu)
+        menuInflater.inflate(R.menu.menu_catalog,menu)
         return true
     }
 
@@ -45,15 +31,10 @@ class CatalogActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
+        return when(item.itemId) {
+            R.id.action_insert_dummy_data -> true
+            R.id.action_delete_all_entries -> true
             else -> super.onOptionsItemSelected(item)
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/sharlene/work/petsfoundation/EditorActivity.kt
+++ app/src/main/java/sharlene/work/petsfoundation/EditorActivity.kt
@@ -1,14 +1,16 @@
 package sharlene.work.petsfoundation
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 public class EditorActivity: AppCompatActivity() {
-    private mNameEditText=EditText
-
+    private val mNameEditText: EditText
     private var mGender=0
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.)
+        setContentView(R.layout.activity_editor)
+
     }
 }
